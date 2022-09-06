6ae44d70cbb5e1e61b45deae768d22530052255e
Sebastien Niang <sebastien.niang@hotmail.fr>
2022-09-06 17:21:15 +0200

    lab done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+const contacts = require("./contacts.json");
+//const contactsToDisplay = contacts.slice(0, 4);
+const init = contacts.slice(0, 4);
 
 function App() {
+  const [contactsToDisplay, setContact] = useState(init);
+
+  const addNewContact = () => {
+    const random = contacts[Math.floor(Math.random() * contacts.length)];
+
+    if (contactsToDisplay.find((e) => e.name === random.name)) return;
+
+    setContact([...contactsToDisplay, random]);
+  };
+  const sortByPop = () => {
+    setContact(() =>
+      [...contactsToDisplay].sort((c1, c2) => c1.popularity < c2.popularity)
+    );
+  };
+  const sortByName = () => {
+    setContact(() =>
+      [...contactsToDisplay].sort((c1, c2) => c1.name.localeCompare(c2.name))
+    );
+  };
+  const removeContact = (name) => {
+    const allContactExceptCurrent = contactsToDisplay.filter(
+      (contact) => contact.name !== name
+    );
+
+    setContact(allContactExceptCurrent);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <button onClick={() => addNewContact()}>ADD NEW RANDOM CONTACT</button>
+      <button onClick={sortByName}>Sort By Name</button>
+      <button onClick={sortByPop}>Sort By Popularity</button>
+      <table className="celibrity-table">
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Populatiry</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+        </tr>
+
+        {contactsToDisplay.map((contact) => {
+          return (
+            <>
+              <tr
+                key={contact.name}
+                onClick={() => removeContact(contact.name)}
+              >
+                <td>
+                  <img src={contact.pictureUrl} alt="profile-picutre" />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+                <td>{contact.wonOscar && "🏆"}</td>
+                <td>{contact.wonEmmy && "🏆"}</td>
+              </tr>
+            </>
+          );
+        })}
+      </table>
     </div>
   );
 }
